657cdbd5cac5577f920def2e0777209e84b611d5
Seu Nome Completo <chfla58@gmail.com>
2025-10-05 09:31:39 -0300

    Adicionar botão 'Imagem' na toolbar do modal para inserir imagens via URL em procedimentos de cliente

--- public/app.js
+++ public/app.js
@@ -38,6 +38,7 @@ const close = document.getElementsByClassName('close')[0];
 const boldBtn = document.getElementById('boldBtn');
 const italicBtn = document.getElementById('italicBtn');
 const underlineBtn = document.getElementById('underlineBtn');
+const imageBtn = document.getElementById('imageBtn');
 const fontSizeSelect = document.getElementById('fontSizeSelect');
 const colorPicker = document.getElementById('colorPicker');
 
@@ -225,6 +226,12 @@ function showModal(title, initialValue, callback) {
 boldBtn.addEventListener('click', () => document.execCommand('bold'));
 italicBtn.addEventListener('click', () => document.execCommand('italic'));
 underlineBtn.addEventListener('click', () => document.execCommand('underline'));
+imageBtn.addEventListener('click', () => {
+    const url = prompt('Digite a URL da imagem:');
+    if (url) {
+        document.execCommand('insertImage', false, url);
+    }
+});
 fontSizeSelect.addEventListener('change', () => document.execCommand('fontSize', false, fontSizeSelect.value));
 colorPicker.addEventListener('change', () => document.execCommand('foreColor', false, colorPicker.value));
 
